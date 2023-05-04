9181e609872e92ff9b186e2c5ec917d81c4735b1
PavloDrabchuk <ravluk2000@gmail.com>
2023-05-04 16:13:08 +0300

    initial commit

---
+++ src/main/java/com/example/blankjavaproject/BlankJavaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.blankjavaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlankJavaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlankJavaProjectApplication.class, args);
+    }
+
+}
